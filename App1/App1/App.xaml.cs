c2c04de00274368bec49e9e44ca6d1ea20472bd4
Aleksandr Kolmogorov <kimkapitan96@gmail.com>
2021-04-16 21:24:47 +0600

    Add project files.

---
+++ App1/App1/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using NFCSample;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
